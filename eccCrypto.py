264fa99b502720f24f76f800c5bf4e0d5fcc123f
LucForster <luc.forster2@web.de>
2022-10-05 23:36:14 +0200

    added eccCrypto.py

--- eccCrypto.py
+++ eccCrypto.py
@@ -1,1 +1,12 @@
+#This class supplies mathods used for ECC Cryptograpie
+class ECCcrypto:
+    def __init__(self):
+        self.testattribut = None
 
+    def eukliedAlgo(self, n, e):
+        r = 1
+        while r != 0:
+            r = n % e
+            n = e
+            e = r
+        return n
--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+from eccCrypto import ECCcrypto
+
 print("Hi, ich werde mal eine Studienarbeit zu Elliptischen Kurven")
+
+crypto = ECCcrypto()
+ggt = crypto.eukliedAlgo(12, 15)
+print(ggt)
