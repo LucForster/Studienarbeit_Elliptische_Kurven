d3befb61bfd3fdf7144922522452f24fa4731c1c
LucForster <luc.forster2@web.de>
2022-10-07 18:56:58 +0200

    added binExponantationInK

--- eccCrypto.py
+++ eccCrypto.py
@@ -1,4 +1,18 @@
-#This class supplies mathods used for ECC Cryptograpie
+# This class supplies mathods used for ECC Cryptograpie
+def square(value, n):
+    temp = value
+    value = value ** 2 % n
+    print(str(temp) + "^2 mod " + str(n) + " = " + str(value))
+    return value
+
+
+def multiply(value, n, a):
+    temp = value
+    value = (value * a) % n
+    print(str(temp) + " * " + str(a) + " mod " + str(n) + " = " + str(value))
+    return value
+
+
 class ECCcrypto:
     def __init__(self):
         self.testattribut = None
@@ -10,3 +24,28 @@ class ECCcrypto:
             n = e
             e = r
         return n
+
+    def binExponantationInK(self, a, k, n):
+        binary = bin(k)
+        length = len(binary) - 2
+        print(str(binary))
+        print(str(length))
+
+        if length == 1:
+            value = a % n
+        else:
+            value = a
+            i = 1
+            while i < length:
+                if binary[i + 2] == "0":
+                    print("0 =>")
+                    value = square(value, n)
+                    print("")
+                else:
+                    print("1 =>")
+                    value = square(value, n)
+                    value = multiply(value, n, a)
+                    print("")
+                i = i + 1
+        print("Solution: " + str(value))
+        return value
