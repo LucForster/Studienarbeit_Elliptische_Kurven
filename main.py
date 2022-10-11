533aa02cd727ed5b2bea13d0e8dd2451501876fa
AaronLelouch <aaron.lacks@outlook.de>
2022-10-11 18:04:48 +0200

    Test

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from eccCrypto import ECCcrypto
+from prim import primes
 
 print("Hi, ich werde mal eine Studienarbeit zu Elliptischen Kurven")
 
 crypto = ECCcrypto()
-ggt = crypto.eukliedAlgo(12, 15)
-print(ggt)
+prime_numbers = primes()
+#ggt = crypto.eukliedAlgo(12, 15)
+#print(ggt)
 crypto.eratosthenes(100000)
+print(primes.checkPrime(100000))
--- prim.py
+++ prim.py
@@ -1,7 +1,6 @@
-'Diese Funktion gibt die Primzahlen bis zu einer eingegebenen Zahl n als Liste zurück'
 def eratosthenes(n):
     primes = []
-    for i in range(2, n+1):
+    for i in range(2, n + 1):
         for j in range(2, i):
             if i % j == 0:
                 break
@@ -9,13 +8,14 @@ def eratosthenes(n):
             primes.append(i)
     return primes
 
-'Diese Funktion prüft, ob eine Zahl eine Primzahl ist und gibt ein True für "Ist eine Primzahl" und ein False für "Ist keine Primzahl" zurück'
-def checkPrime(n):
-    primes = eratosthenes(n+1)
-    end = False
-    for x in primes:
-        if x == n:
-            end = True
-    return end
+class primes:
+    'Diese Funktion gibt die Primzahlen bis zu einer eingegebenen Zahl n als Liste zurück'
 
-print(checkPrime(99988))
+    'Diese Funktion prüft, ob eine Zahl eine Primzahl ist und gibt ein True für "Ist eine Primzahl" und ein False für "Ist keine Primzahl" zurück'
+    def checkPrime(n):
+        primes = eratosthenes(n+1)
+        end = False
+        for x in primes:
+            if x == n:
+                end = True
+        return end
